dd7225563f7b3cc7a24e7845457cbdedfaa20afa
Lei Wang <ssst0n3@gmail.com>
2021-05-19 13:34:53 +0800

    add

--- linux/clean_google-chrome_cache.sh
+++ linux/clean_google-chrome_cache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # https://chromium.googlesource.com/chromium/src/+/master/docs/user_data_dir.md
-rm -r ~/.cache/google-chrome/Default
+rm -rf ~/.cache/google-chrome/Default
+rm -rf ~/.config/google-chrome/Default/Service\ Worker/CacheStorage/
