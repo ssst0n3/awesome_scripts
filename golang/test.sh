19bc722b87faa5ea7cb7e67e3361953a730ed5ba
Lei Wang <ssst0n3@gmail.com>
2021-06-21 14:21:39 +0800

    Stop on first test failure

--- golang/test.sh
+++ golang/test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-go test -count=1 -parallel 1 -v ./...
+# go test -count=1 -parallel 1 -v ./...
+
+
+# https://github.com/golang/go/issues/33038#issuecomment-514410406
+for s in $(go list ./...); do if ! go test -failfast -v -p 1 $s; then break; fi; done
+
